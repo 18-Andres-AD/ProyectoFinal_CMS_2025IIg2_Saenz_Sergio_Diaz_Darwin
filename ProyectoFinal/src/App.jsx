cef656b7c4ad0838e4a40513bbc3d16a1c6375ba
DarwinAndresDiaz <18-Andres-AD>
2025-11-06 14:39:30 -0500

    loguin

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/NavBar/Navbar";
 
-export  function App() {
-  return (    
-    <Routes>
-      <Navbar />
-      {/* <div className="container mt-3">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/characters" element={<Characters />} />
-          <Route path="/episodes" element={<Episodes />} />
-          <Route path="/locations" element={<Locations />} />
-        </Routes>
-      </div> */}
-    </Routes>
-);
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loguin from "./Pages/Loguin/Loguin";
+import './index.css'
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Loguin />} />
+        {/* <Route path="/inicio" element={<Inicio />} /> */}
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;

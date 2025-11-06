7e198f092ff3b63cba90fcefa434987b43dd8d11
DarwinAndresDiaz <18-Andres-AD>
2025-11-06 16:10:25 -0500

    Loguin

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -2,12 +2,14 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loguin from "./Pages/Loguin/Loguin";
+import RecuperarContrasena from "./Pages/Restablecer/Restablecer";
 import './index.css'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Loguin />} />
+        <Route path="/Restablecer" element={<RecuperarContrasena />} />
         {/* <Route path="/inicio" element={<Inicio />} /> */}
       </Routes>
     </Router>
